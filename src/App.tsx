7e817079b73159d503900d853885527f095edd81
windowtranslator <windowtranslator@outlook.com>
2026-05-22 13:43:10 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import viteLogo from './assets/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div></div>
+      <a href="https://vitejs.dev" target="_blank">
+        <img src={viteLogo} className="logo" alt="Vite logo" />
+      </a>
+    </>
+  )
+}
+
+export default App
